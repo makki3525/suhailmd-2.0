d55503a9ca9ff6099f6ac8a4318d13a351f0e566
makki3525 <111455780+makki3525@users.noreply.github.com>
2024-11-19 12:51:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.caption = process.env.CAPTION || global.caption || "© BatmanMD"
 
 
 global.devs = "" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "923245643588,923257607934,923005705557";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "923245643588,923257607934,923005705557,923190446934";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923245643588";
 
 
